5bef4e7020c79c058c3176af7caef319437acfdc
Gianluca Bianchi <73002791+gianbianchi@users.noreply.github.com>
2021-11-09 15:43:46 -0300

    Teste

--- src/main/java/com/epiousion/minhasfinancas/MinhasfinancasApplication.java
+++ src/main/java/com/epiousion/minhasfinancas/MinhasfinancasApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MinhasfinancasApplication {
-
+//Teste
 	public static void main(String[] args) {
 		SpringApplication.run(MinhasfinancasApplication.class, args);
 	}
